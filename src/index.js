c62ed02668b54d5235a8c78bc7b920968b5b7f15
Jamie Halvorson <jamie@halvorson.co.uk>
2018-02-28 20:27:12 +0000

    It works

--- src/index.js
+++ src/index.js
@@ -9,43 +9,8 @@ const resolvers = {
     twitter() { return data.twitter },
     projects() { return data.projects },
     sideProjects() { return data.sideProjects }
-    // feed(parent, args, ctx, info) {
-    //   return ctx.db.query.posts({ where: { isPublished: true } }, info)
-    // },
-    // drafts(parent, args, ctx, info) {
-    //   return ctx.db.query.posts({ where: { isPublished: false } }, info)
-    // },
-    // post(parent, { id }, ctx, info) {
-    //   return ctx.db.query.post({ where: { id } }, info)
-    // },
-  },
-  Mutation: {
-    createDraft(parent, { title, text }, ctx, info) {
-      return ctx.db.mutation.createPost(
-        {
-          data: {
-            title,
-            text,
-            isPublished: false,
-          },
-        },
-        info,
-      )
-    },
-    deletePost(parent, { id }, ctx, info) {
-      return ctx.db.mutation.deletePost({ where: { id } }, info)
-    },
-    publish(parent, { id }, ctx, info) {
-      return ctx.db.mutation.updatePost(
-        {
-          where: { id },
-          data: { isPublished: true },
-        },
-        info,
-      )
-    },
-  },
-}
+  }
+};
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
@@ -54,9 +19,9 @@ const server = new GraphQLServer({
     ...req,
     db: new Prisma({
       typeDefs: 'src/generated/prisma.graphql',
-      endpoint: 'http://localhost:4466/halvorson.co.uk-blog-yoga/dev', // the endpoint of the Prisma DB service
-      secret: 'mysecret123', // specified in database/prisma.yml
-      debug: true, // log all GraphQL queryies & mutations
+      endpoint: process.env.PRISMA_ENDPOINT,
+      secret: process.env.PRISMA_SECRET,
+      debug: true,
     }),
   }),
 })
